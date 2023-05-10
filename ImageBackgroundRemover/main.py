013dd81b3d203bd0a61835db495508b81bbf9da3
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2023-05-10 12:11:14 +0300

    Update

--- ImageBackgroundRemover/main.py
+++ ImageBackgroundRemover/main.py
@@ -3,8 +3,8 @@ from rembg import remove
 input_path = "cr7.jpg"
 output_path = "output.png" #should be png
 
-with open(input_path, 'rb') as i: #rb --> read by byte
-    with open(output_path, 'wb') as a: #wb --> erite bye byte
+with open(input_path, 'rb') as i: #rb --> read as a byte
+    with open(output_path, 'wb') as a: #wb --> write as a byte
         input_file = i.read() #read from input_path
         output_file = remove(input_file) #remove background
-        a.write(output_file) #write in to output_file
+        a.write(output_file) #write into output_file
