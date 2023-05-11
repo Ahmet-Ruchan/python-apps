b4670fc9cf85bf170d41b0d7f5365ba0993f102b
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2023-05-11 18:00:46 +0300

    Delete YouTubeDownloader directory

--- YouTubeDownloader/main.py
+++
@@ -1,5 +0,0 @@
-import pytube
-
-url = input("Enter video url: ")
-path = "C:/Users/LENOVO/Desktop/HelloPython/YouTubeDownloader/Videos"
-pytube.YouTube(url).streams.get_highest_resolution().download(path)
