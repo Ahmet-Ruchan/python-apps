37c6eb0b648fd45e70687f35211de25bfd127f1b
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2023-05-11 18:27:00 +0300

    Update

--- QR Code Creater/main.py
+++ QR Code Creater/main.py
@@ -2,4 +2,4 @@ import pyqrcode
 
 url = input("Enter url to generate qr code: ")
 qr_code = pyqrcode.create(url)
-qr_code.svg("qrcode.svg",scale=10) #scale = boyutu
+qr_code.svg("qrcode.svg",scale=10) 
